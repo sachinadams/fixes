bad9379c5925f4cd1682643266c24ffa90651463
sachinadams <sachin.prasad@gmail.com>
2019-05-08 21:25:39 -0500

    Update metapatch.sh
    correction to way xmeta docker image is instantiated for updates. The default entry script makes immutable changes to the container which cannot be used after commits.

--- metapatch.sh
+++ metapatch.sh
@@ -1,6 +1,7 @@
 #Run in master
 docker rm --force "/xmetapatch" > /dev/null 2>&1
-docker run -dit --name=xmetapatch  mycluster.icp:8500/zen/is-db2xmeta-image:11.7.0.2SP1
+#docker run -dit --name=xmetapatch  mycluster.icp:8500/zen/is-db2xmeta-image:11.7.0.2SP1
+docker run -dit --name=xmetapatch --entrypoint "tail" mycluster.icp:8500/zen/is-db2xmeta-image:icp4d-1.2.1.1-rc.2   -f  /dev/null
 docker exec xmetapatch chage -m 0 -M 99999 -I -1 -E -1 xmeta
 docker exec xmetapatch chage -m 0 -M 99999 -I -1 -E -1 db2inst1
 docker exec xmetapatch chage -m 0 -M 99999 -I -1 -E -1 db2fenc1
@@ -13,13 +14,15 @@ docker exec xmetapatch touch /patched.000
 docker tag mycluster.icp:8500/zen/is-db2xmeta-image:11.7.0.2SP1  mycluster.icp:8500/zen/is-db2xmeta-image:11.7.0.2SP1.org
 docker commit xmetapatch mycluster.icp:8500/zen/is-db2xmeta-image:11.7.0.2SP1
 docker push mycluster.icp:8500/zen/is-db2xmeta-image:11.7.0.2SP1
-kubectl  scale rs  $(kubectl get rs -n zen | grep xmetarepo | cut -f1 -d' ' | tr '\n' ' ') -n zen --replicas=0
+#kubectl  scale rs  $(kubectl get rs -n zen | grep xmetarepo | cut -f1 -d' ' | tr '\n' ' ') -n zen --replicas=0
+kubectl scale  Deployment/ibm-iisee-zen100-ibm-iisee-zen-iis-xmetarepo -n zen   --replicas=0
 
 # This needs to be done on all the worker nodes. 
 docker rmi mycluster.icp:8500/zen/is-db2xmeta-image:11.7.0.2SP1 --force
 
 #This would be needed in the master
-kubectl  scale rs  $(kubectl get rs -n zen | grep xmetarepo | cut -f1 -d' ' | tr '\n' ' ') -n zen --replicas=1
+#kubectl  scale rs  $(kubectl get rs -n zen | grep xmetarepo | cut -f1 -d' ' | tr '\n' ' ') -n zen --replicas=1
+kubectl scale  Deployment/ibm-iisee-zen100-ibm-iisee-zen-iis-xmetarepo -n zen   --replicas=1
 #kubectl delete pod $(kubectl get pods -n zen | grep iis | cut -f1 -d' ' | tr '\n' ' ') -n zen
 sleep 15
 echo "Current User Expiry------------------------------------------"
