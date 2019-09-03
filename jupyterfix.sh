9490fa48f6d6b2ed5450593f800603dee4cf0c8e
sachinadams <sachin.prasad@gmail.com>
2019-09-03 12:00:52 -0500

    Update jupyterfix.sh

--- jupyterfix.sh
+++ jupyterfix.sh
@@ -15,16 +15,16 @@ kubectl scale  deployment -l type=jupyter-py36 --replicas=0
 # This needs to be done on all the worker nodes. 
 echo " " 
 echo " " 
-echo "\n+++ Below command needs to be run manually in all the worker nodes. +++ \n "
+echo "+++ Below command needs to be run manually in all the worker nodes. +++ "
 echo "docker rmi mycluster.icp:8500/zen/jupyter-d9a36-set1:v1.0.157-x86_64    --force"
 
 #This would be needed in the master
 echo " " 
 echo " "
-echo "\n+++ Once done, run below command from master +++ \n"
+echo "+++ Once done, run below command from master +++ "
 echo "kubectl scale  deployment -l type=jupyter-py36  --replicas=1"
 
 echo " " 
 echo "  "
-echo "\n+++ After scaling back the deployment, make sure the jupyter pods is up and then check if the change is present in the util.py file by running +++\n"
+echo "+++ After scaling back the deployment, make sure the jupyter pods is up and then check if the change is present in the util.py file by running +++"
 echo "for i in `kubectl get pods -n zen | grep jupyter-py36-server |cut -f1 -d" "`; do kubectl exec -n zen  $i --  tail -1 /opt/conda3/lib/python3.6/site-packages/dsx_ml/util.py ;  done"
